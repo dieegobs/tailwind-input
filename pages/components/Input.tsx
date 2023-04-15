f707315c354d8eb9fc38e63e63ba5744b457e201
dieegobs <diegohevant@gmail.com>
2023-04-14 23:57:02 -0300

    Finalizando

--- pages/components/Input.tsx
+++ pages/components/Input.tsx
@@ -6,15 +6,15 @@ export default function Input(props: InputHTMLAttributes<HTMLInputElement>) {
         
         <input   {...props} 
         className="peer rounded-md w-full px-4 py-3 text-sm text-gray-700 border-gray-200 border-2 focus:bg-white bg-white
-        placeholder:bg-gray-200 placeholder-shown:border-none placeholder-transparent
+        placeholder-shown:bg-gray-200 placeholder-shown:border-none placeholder-transparent
         "/>
 
         <label htmlFor={props?.id} 
-        className="absolute left-2 -top-2.5 px-1 text-sm transition-all bg-white rounded text-gray-500
+        className="absolute left-2 -top-2.5 px-1 text-sm transition-all bg-white rounded text-gray-700
         peer-placeholder-shown:text-sm peer-placeholder-shown:text-gray-400 
         peer-placeholder-shown:top-3 peer-placeholder-shown:left-4 peer-placeholder-shown:bg-gray-200
         peer-focus:-top-2.5 peer-focus:left-2 peer-focus:text-blue-500 peer-focus:bg-white peer-focus:font-semibold
-        
+
         "> {props?.placeholder}</label>
 
      </div>
