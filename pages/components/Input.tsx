3d2509a06ae403ddef6981a31d4a0538eaddc95e
dieegobs <diegohevant@gmail.com>
2023-04-14 23:52:28 -0300

    Fazendo o input funcional

--- pages/components/Input.tsx
+++ pages/components/Input.tsx
@@ -1,9 +1,23 @@
+import { InputHTMLAttributes } from "react";
 
-export default function Input() {
+export default function Input(props: InputHTMLAttributes<HTMLInputElement>) {
     return (
-     
-     <input type="text" />
-     
+     <div className="relative mt-2">
+        
+        <input   {...props} 
+        className="peer rounded-md w-full px-4 py-3 text-sm text-gray-700 border-gray-200 border-2 focus:bg-white bg-white
+        placeholder:bg-gray-200 placeholder-shown:border-none placeholder-transparent
+        "/>
+
+        <label htmlFor={props?.id} 
+        className="absolute left-2 -top-2.5 px-1 text-sm transition-all bg-white rounded text-gray-500
+        peer-placeholder-shown:text-sm peer-placeholder-shown:text-gray-400 
+        peer-placeholder-shown:top-3 peer-placeholder-shown:left-4 peer-placeholder-shown:bg-gray-200
+        peer-focus:-top-2.5 peer-focus:left-2 peer-focus:text-blue-500 peer-focus:bg-white peer-focus:font-semibold
+        
+        "> {props?.placeholder}</label>
+
+     </div>
     );
   }
   
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,16 +2,16 @@ import Input from "./components/Input";
 
 export default function Home() {
   return (
-   
-    <div className="">
 
-      <h1>Clones de Input</h1>
-      <div>
-       <Input />
-       <Input />
+    <div className="flex flex-col justify-center items-center mt-12">
+
+      <h1 className="text-gray-700 font-bold text-2xl mb-6 ">Clones de Input</h1>
+      <div className="flex flex-col gap-3 mt-2 w-96">
+        <Input id="name" type="text" placeholder="Nome" required={true}/>
+        <Input id="email" type="email" placeholder="Email" required={true}/>
       </div>
 
     </div>
-   
+
   );
 }
